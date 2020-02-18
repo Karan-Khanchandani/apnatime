856863a720c644dbe464d7cf508a708561699123
Karan Khanchandani <karan.khanchandani@sixt.com>
2020-02-18 15:06:06 +0530

    Defines database entities

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,9 @@
+package entity;
+
+import java.time.OffsetDateTime;
+
+public class User {
+    private Integer userId;
+    private String name;
+    private OffsetDateTime creationDate;
+}
---
+++ src/main/java/entity/UserFriends.java
@@ -0,0 +1,9 @@
+package entity;
+
+import java.time.OffsetDateTime;
+
+public class UserFriends {
+    private Integer userId;
+    private Integer friendId;
+    private OffsetDateTime connectionDate;
+}
