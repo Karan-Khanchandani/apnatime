d6721fbcb6326731e4456a88554fcba4243c3987
Karan Khanchandani <karan.khanchandani@sixt.com>
2020-02-18 19:17:38 +0530

    Adds sql for creating database and adds configuration

---
+++ src/main/resources/db/migration/V1__create_statements.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER NOT NULL,
+    name varchar(255),
+    creation_date timestamp with time zone default now(),
+    PRIMARY KEY(user_id)
+
+);
+
+CREATE INDEX IF NOT EXISTS user_id_index on users USING btree (user_id);
+
+
+CREATE TABLE IF NOT EXISTS user_friends_mapping(
+    user_id INTEGER NOT NULL,
+    friend_id INTEGER NOT NULL,
+    connection_date timestamp with time zone default now(),
+    PRIMARY KEY(user_id,friend_id)
+);
+
+CREATE INDEX IF NOT EXISTS user_id_friend_id_index on user_friends_mapping USING btree (user_id, friend_id);
+
+
